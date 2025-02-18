d64a5e901108bdd2c07b61b46f27a5f34e025652
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-02-18 16:30:21 +0100

    Add the ftplugin for `gitcommit`

--- home/vim/default.nix
+++ home/vim/default.nix
@@ -16,12 +16,18 @@
   xdg.stateFile."vim/swap/.keep".text = "";
   xdg.stateFile."vim/undo/.keep".text = "";
 
-  # NOTE: `noexpandtab` is already set in the default ftplugin for make
-  xdg.configFile."vim/after/ftplugin/make.vim".text = ''
-    set tabstop=4
+  xdg.configFile."vim/after/ftplugin/gitcommit.vim".text = ''
+    setlocal colorcolumn=51,73
+    setlocal spell
+    setlocal textwidth=72
   '';
 
   xdg.configFile."vim/after/ftplugin/just.vim".text = ''
-    set tabstop=4
+    setlocal tabstop=4
+  '';
+
+  # NOTE: `noexpandtab` is already set in the default ftplugin for make
+  xdg.configFile."vim/after/ftplugin/make.vim".text = ''
+    setlocal tabstop=4
   '';
 }
--- nixvim/ftplugins.nix
+++ nixvim/ftplugins.nix
@@ -2,15 +2,23 @@
 Custom ftplugins with configurtion for specific file types.
 */
 {...}: {
-  files."after/ftplugin/make.lua" = {
+  files."after/ftplugin/gitcommit.lua" = {
     localOpts = {
-      # NOTE: `noexpandtab` is already set in the default ftplugin for make
-      tabstop = 4;
+      colorcolumn = [51 73];
+      spell = true;
+      textwidth = 72;
     };
   };
 
   files."after/ftplugin/just.lua" = {
     localOpts = {
+      tabstop = 4;
+    };
+  };
+
+  files."after/ftplugin/make.lua" = {
+    localOpts = {
+      # NOTE: `noexpandtab` is already set in the default ftplugin for make
       tabstop = 4;
     };
   };
