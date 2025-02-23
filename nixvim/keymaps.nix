e88ec015e1fdf7a9cda6fc6111bccccc5eceb794
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-02-23 23:02:38 +0100

    Clean up and extend (Neo)Vim keymaps

--- nixvim/keymaps.nix
+++ nixvim/keymaps.nix
@@ -5,15 +5,35 @@
   };
 
   keymaps = [
+    # Enter the command mode with `;`
     {
-      mode = "n";
+      mode = ["n" "x"];
       key = ";";
       action = ":";
     }
     {
-      mode = "n";
+      mode = ["n" "x"];
       key = ":";
       action = ";";
+    }
+
+    # Maintain visual selection when changing indentation
+    {
+      mode = "x";
+      key = "<";
+      action = "<gv";
+    }
+    {
+      mode = "x";
+      key = ">";
+      action = ">gv";
+    }
+
+    # Stop the highlighting of search matches
+    {
+      mode = "n";
+      key = "<Esc>";
+      action = "<Cmd>nohlsearch<CR>";
     }
   ];
 }
