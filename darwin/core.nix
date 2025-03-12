ad8f9dd8d3b28b640968104745ebf15602f12e92
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-03-12 15:48:14 +0100

    Remove the nix-darwin option for nix-daemon
    This option is no longer used in the newer version of nix-darwin.

--- darwin/core.nix
+++ darwin/core.nix
@@ -6,7 +6,4 @@ _: {
 
   # The platform the configuration will be used on
   nixpkgs.hostPlatform = "x86_64-darwin";
-
-  # Auto upgrade nix package and the daemon service
-  services.nix-daemon.enable = true;
 }
