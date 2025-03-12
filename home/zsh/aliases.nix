5b58a30961b8db3edc1ff58616367cf36b2280d8
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-03-12 17:23:17 +0100

    Allow for optional ZSH aliases

--- home/zsh/aliases.nix
+++ home/zsh/aliases.nix
@@ -1,55 +1,61 @@
-_: {
-  programs.zsh.shellAliases = {
-    # GENERAL
+{
+  config,
+  lib,
+  ...
+}: {
+  programs.zsh.shellAliases = lib.mkMerge [
+    {
+      # GENERAL
 
-    # Clear the terminal output
-    cl = "clear";
+      # Clear the terminal output
+      cl = "clear";
 
-    # APPLICATIONS
+      # APPLICATIONS
+
+      # Use eza (with some defaults) instead of ls and tree
+      ls = "eza --icons --hyperlink --group-directories-first";
+      ll = "ls --long --all --time-style=long-iso --git";
+      lt = ''ll --tree --ignore-glob ".git"'';
+      tree = "ls --tree --level 3";
+
+      # Open the default editor (either Vim or Neovim)
+      v = "\${EDITOR:-vim}";
+
+      # PYTHON
+
+      py = "python";
+      cl-pyc = "find ./ -name '*.pyc' -delete";
+
+      # GIT
+
+      ga = "git add";
+      gb = "git branch";
+      gc = "git checkout";
+      gd = "git diff";
+      gf = "git fetch";
+      gl = "git log";
+      gr = "git restore";
+      gs = "git status";
+      gcm = "git commit";
+      gds = "gd --staged";
+      glg = "git log --graph";
+      gll = "git log --oneline";
+      gpl = "git pull";
+      gps = "git push";
+      grr = "git reset";
+      grs = "gr --staged";
+      gst = "git stash";
+
+      # DOCKER
+
+      dps = "docker ps";
+      dcp = "docker-compose";
+      cl-docker = "docker system prune --volumes -f";
+    }
 
     # Use bat instead of cat
-    cat = "bat";
+    (lib.mkIf config.programs.bat.enable {cat = "bat";})
 
-    # Use eza (with some defaults) instead of ls and tree
-    ls = "eza --icons --hyperlink --group-directories-first";
-    ll = "ls --long --all --time-style=long-iso --git";
-    lt = ''ll --tree --ignore-glob ".git"'';
-    tree = "ls --tree --level 3";
-
-    # Open the default editor (either Vim or Neovim)
-    v = "\${EDITOR:-vim}";
-
-    lg = "lazygit";
-
-    # PYTHON
-
-    py = "python";
-    cl-pyc = "find ./ -name '*.pyc' -delete";
-
-    # GIT
-
-    ga = "git add";
-    gb = "git branch";
-    gc = "git checkout";
-    gd = "git diff";
-    gf = "git fetch";
-    gl = "git log";
-    gr = "git restore";
-    gs = "git status";
-    gcm = "git commit";
-    gds = "gd --staged";
-    glg = "git log --graph";
-    gll = "git log --oneline";
-    gpl = "git pull";
-    gps = "git push";
-    grr = "git reset";
-    grs = "gr --staged";
-    gst = "git stash";
-
-    # DOCKER
-
-    dps = "docker ps";
-    dcp = "docker-compose";
-    cl-docker = "docker system prune --volumes -f";
-  };
+    (lib.mkIf config.programs.lazygit.enable {lg = "lazygit";})
+  ];
 }
