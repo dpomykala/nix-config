d4f4009cc4d9d7ab9245c0900b15961f4b5e69be
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-03-26 13:26:17 +0100

    Disable gO/go mappings from mini.basics
    These mappings can be replaced by the built-in mappings [ + <Space> and
    ] + <Space>.

--- nixvim/plugins/mini/basics.nix
+++ nixvim/plugins/mini/basics.nix
@@ -1,13 +1,21 @@
 _: {
-  plugins.mini.modules.basics = {
-    # Manage options manually
-    options.basic = false;
+  plugins.mini = {
+    modules.basics = {
+      # Manage options manually
+      options.basic = false;
 
-    /*
-    Set additional mappings for handling windows:
-    - <C-jhkl> for window navigation
-    - <C-arrow> for window resizing
-    */
-    mappings.windows = true;
+      /*
+      Set additional mappings for handling windows:
+      - <C-jhkl> for window navigation
+      - <C-arrow> for window resizing
+      */
+      mappings.windows = true;
+    };
+
+    # Disable gO/go mappings - can be replaced with built-in [ / ] + <Space>
+    luaConfig.post.__raw = ''
+      vim.keympa.del("n", "gO")
+      vim.keympa.del("n", "go")
+    '';
   };
 }
