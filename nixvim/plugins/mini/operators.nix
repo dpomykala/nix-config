99f927919561d996f81fa460fb558450a0453286
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-03-26 13:30:23 +0100

    Remap the `gx` mapping to `go` if mini.operators used

--- nixvim/plugins/mini/operators.nix
+++ nixvim/plugins/mini/operators.nix
@@ -1,7 +1,17 @@
-_: {
+{lib, ...}: {
   plugins.mini.modules.operators = {
     # The `gs` prefix conflicts with custom mappings for mini.surround
     sort.prefix = "g\\";
-    # TODO: Replace gO/go with built-in keymaps [<Space> and ]<Space> ?
   };
+
+  keymaps = let
+    inherit (lib.custom.nixvim) nMap;
+  in [
+    # Remap the built-in `gx` mapping to `go` as `gx` is used by mini.operators
+    (nMap {
+      key = "go";
+      action = "gx";
+      desc = "";
+    })
+  ];
 }
