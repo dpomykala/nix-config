6f0c9661f76e60209b957ad66edb1086dceafe08
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-05-25 00:21:15 +0200

    Use a sandboxed environment for builds

--- system/darwin/nix.nix
+++ system/darwin/nix.nix
@@ -2,5 +2,8 @@ _: {
   nix.settings = {
     # Use new-style commands and flakes
     experimental-features = "nix-command flakes";
+
+    # Use a sandboxed environment for builds
+    sandbox = true;
   };
 }
