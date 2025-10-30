96f2846803c127c5bc1bbb14bb22a402e10e7831
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-10-30 23:59:57 +0100

    Use multiple GitHub accounts via SSH

--- home/configs/dp@mbp-16/default.nix
+++ home/configs/dp@mbp-16/default.nix
@@ -7,4 +7,20 @@ _: {
     ../../users/dp.nix
     ./organize-tool
   ];
+
+  programs.ssh = {
+    # Rules for specific hosts
+    # The order of blocks is not guaranteed unless DAG functions are used
+    matchBlocks = {
+      # Default GitHub account (personal)
+      "github.com gh" = {
+        identityFile = "~/.ssh/id_ed25519_github_personal.pub";
+      };
+
+      # Secondary GitHub account (work)
+      "github.com-ll gh-ll" = {
+        identityFile = "~/.ssh/id_ed25519_github_work_ll.pub";
+      };
+    };
+  };
 }
