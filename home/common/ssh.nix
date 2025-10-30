d2a88f08aa4e337c2665e37c7cfbe286cb88a339
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-10-30 16:35:29 +0100

    Use identityAgent option to set up SSH agent

--- home/common/ssh.nix
+++ home/common/ssh.nix
@@ -4,6 +4,7 @@ This setup allows for managing all SSH keys using 1Password.
 Requirements:
   1. The 1Password desktop app must be installed.
   2. The SSH agent must be enabled in the 1Password app.
+  3. Identity files must be available (if specified in config)
 
 WARN: Installing 1Password GUI through Home Manager is not reliable.
 It requires the setuid permission for the chrome-sandbox executable,
@@ -15,12 +16,19 @@ On generic Linux, install manually using your preferred method.
 
 For more information see: https://developer.1password.com/docs/ssh
 */
-{pkgs, ...}: let
-  ssh_agent_socket =
+{
+  lib,
+  pkgs,
+  ...
+}: let
+  inherit (lib.strings) escape;
+
+  ssh_agent_socket = escape [" "] (
     if pkgs.stdenv.isLinux
     then "~/.1password/agent.sock"
     # For Darwin hosts
-    else "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock";
+    else "~/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+  );
 in {
   home.sessionVariables.SSH_AUTH_SOCK = ssh_agent_socket;
 
@@ -28,7 +36,7 @@ in {
   # See: https://developer.1password.com/docs/ssh/agent/config
   xdg.configFile."1Password/ssh/agent.toml".text = ''
     [[ssh-keys]]
-    # Add all SSH keys from an account with this sign-in address
+    # Add all SSH keys from the account with this sign-in address
     account = "my.1password.com"
   '';
 
@@ -43,23 +51,13 @@ in {
       "*" = {
         identityAgent = ssh_agent_socket;
         # Only use the identities specified explicitly in the config file
-        identitiesOnly = true;
+        # identitiesOnly = true;
       };
-      # # Common configuration for all GitHub hosts
-      # "github.com* gh*" = {
-      #   hostname = "github.com";
-      #   user = "git";
-      # };
-      #
-      # # Default GitHub account (e.g. for work)
-      # "github.com gh" = {
-      #   identityFile = "~/.ssh/id_ed25519_github_work.pub";
-      # };
-      #
-      # # Secondary GitHub account (e.g. personal)
-      # "github.com-personal gh-personal" = {
-      #   identityFile = "~/.ssh/id_ed25519_github_personal.pub";
-      # };
+
+      "github.com* gh*" = {
+        hostname = "github.com";
+        user = "git";
+      };
     };
   };
 }
