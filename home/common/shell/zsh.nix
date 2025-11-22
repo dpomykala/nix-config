56986100c6860eb12ba650f4eb56e4c978e583fd
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-11-22 19:32:41 +0100

    Add the fzf-tab plugin for ZSH
    Re-factor the `zsh.nix` module.
    Enable changing the cursor based on the mode (zsh-vi-mode).

--- home/common/shell/zsh.nix
+++ home/common/shell/zsh.nix
@@ -1,5 +1,6 @@
 {
   config,
+  lib,
   pkgs,
   ...
 }: {
@@ -7,39 +8,65 @@
     enable = true;
     enableCompletion = true;
 
+    autocd = true;
+
+    autosuggestion.enable = true;
+
     dotDir = "${config.xdg.configHome}/zsh";
 
-    autocd = true;
+    initContent = let
+      dirPreview = lib.concatStringsSep " " [
+        "eza"
+        "--color=always"
+        "--icons"
+        "--hyperlink"
+        "--group-directories-first"
+        "--tree"
+        "--level 3"
+        "$realpath"
+      ];
+    in
+      # Configure fzf-tab completion BEFORE compinit
+      lib.mkIf config.programs.fzf.enable (lib.mkOrder 550 ''
+        # Disable sort when completing `git checkout`
+        zstyle ':completion:*:git-checkout:*' sort false
+
+        # Use FZF_DEFAULT_OPTS for fzf-tab
+        zstyle ':fzf-tab:*' use-fzf-default-opts yes
+
+        # Preview directory's content when completing `cd`
+        zstyle ':fzf-tab:complete:cd:*' fzf-preview '${dirPreview}'
+      '');
+
     history = {
-      # Save the history file in the XDG Data Home directory
-      path = "${config.xdg.dataHome}/zsh/zsh_history";
       # Use the extended format for history entries
       extended = true;
+
+      # Save the history file in the XDG Data Home directory
+      path = "${config.xdg.dataHome}/zsh/zsh_history";
     };
 
     # Local variables added at the top of .zshrc
     localVariables = {
+      # Disable changing the cursor based on the mode (zsh-vi-mode)
+      # ZVM_CURSOR_STYLE_ENABLED = false;
+
       # Initialize zsh-vi-mode plugin when sourcing the script
       # This prevents overriding fzf key bindings (ctrl-r)
       # fzf must be sourced AFTER zsh-vi-mode
       # https://github.com/jeffreytse/zsh-vi-mode?tab=readme-ov-file#initialization-mode
       ZVM_INIT_MODE = "sourcing";
-      # Disable changing the cursor based on the mode (zsh-vi-mode)
-      ZVM_CURSOR_STYLE_ENABLED = false;
     };
 
-    # Extra configuration added to .zshrc
-    initContent = ''
-      # Use Vim key bindings
-      setopt VI
-    '';
-
-    # Plugins natively supported by home-manager
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    # Other plugins
     plugins = with pkgs; [
+      # https://github.com/Aloxaf/fzf-tab
+      # Requires fzf to be installed
+      (
+        lib.mkIf config.programs.fzf.enable {
+          inherit (zsh-fzf-tab) src;
+          name = "fzf-tab";
+        }
+      )
       # https://github.com/jeffreytse/zsh-vi-mode
       {
         inherit (zsh-vi-mode) src;
@@ -51,5 +78,12 @@
         name = "zsh-you-should-use";
       }
     ];
+
+    setOptions = [
+      # Use Vim key bindings
+      "VI"
+    ];
+
+    syntaxHighlighting.enable = true;
   };
 }
