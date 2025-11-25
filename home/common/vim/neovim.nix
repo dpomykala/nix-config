19b13be935dd48d2b2ae01f8b370b64c988650be
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-11-25 19:48:29 +0100

    Replace the `system` alias with `stdenv.hostPlatform.system`

--- home/common/vim/neovim.nix
+++ home/common/vim/neovim.nix
@@ -6,7 +6,7 @@
   home = {
     packages = [
       # Use the custom Neovim (NixVim) configuration
-      self.inputs.nix-nvim.packages.${pkgs.system}.default
+      self.inputs.nix-nvim.packages.${pkgs.stdenv.hostPlatform.system}.default
     ];
 
     # Set Neovim as the default editor
