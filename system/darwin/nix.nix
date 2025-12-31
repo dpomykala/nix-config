95a2e742fa894f2f1fd7d65860ff5779ac3876d3
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2025-12-31 13:33:12 +0100

    Add the primaryUser to trusted-users on Darwin

--- system/darwin/nix.nix
+++ system/darwin/nix.nix
@@ -1,9 +1,13 @@
-_: {
+{config, ...}: {
   nix.settings = {
     # Use new-style commands and flakes
     experimental-features = "nix-command flakes";
 
     # Use a sandboxed environment for builds
     sandbox = true;
+
+    # Specify users having additional rights when connecting to the Nix daemon
+    # This allows the use of restricted options, e.g. `--option sandbox false`
+    trusted-users = [config.system.primaryUser];
   };
 }
