3852fd4e104ef6f493407e6f9ac451b66d604efd
Damian Pomykała <168227269+dpomykala@users.noreply.github.com>
2026-01-06 22:24:27 +0100

    Add more tools for mise

--- home/common/mise.nix
+++ home/common/mise.nix
@@ -3,17 +3,20 @@ _: {
     enable = true;
 
     globalConfig = {
-      tools = {
-        python = "latest";
-        # elixir = "latest";
-        # erlang = "latest";
-      };
-
-      # FIXME: Use programs.mise.settings instead?
-      # See: https://nix-community.github.io/home-manager/options.xhtml#opt-programs.mise.settings
       settings = {
         # Load environment variables from dotenv files
         env_file = ".env";
+      };
+
+      tools = {
+        bun = "latest";
+        elixir = "latest";
+        erlang = "latest";
+        go = "latest";
+        node = "latest";
+        python = "latest";
+        rust = "latest";
+        zig = "latest";
       };
     };
   };
