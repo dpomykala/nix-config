0fbee89482702f424069b2234e75e2fd727b03ae
dp <168227269+dpomykala@users.noreply.github.com>
2026-09-21 15:42:57 +0200

    Overwrite an existing config for Karabiner-Elements

--- modules/features/karabiner/default.nix
+++ modules/features/karabiner/default.nix
@@ -5,6 +5,11 @@ _: {
 
   flake.modules.homeManager.karabiner = {
     # WARN: Any configuration changes via the GUI will overwrite the symlink
-    xdg.configFile."karabiner/karabiner.json".source = ./karabiner.json;
+    xdg.configFile."karabiner/karabiner.json" = {
+      # Overwrite the current config file if exists
+      force = true;
+
+      source = ./karabiner.json;
+    };
   };
 }
